3ab9ec9c3984c9a1b407b8a6626692baac5fc6fa
jose <jjj.jose94@gmail.com>
2022-05-26 17:29:55 -0500

    v4.1 next step is the persit

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -11,15 +11,21 @@ const Game = () => {
 	const [xisNext, setXIsNext] = useState(true);
 	const winner = calculateWinner(history[stepNum]);
 	const player = xisNext ? 'X' : 'O';
+
+	//zustand score counter
 	const count = useStore((state) => state.count);
 	const setCount = useStore((state) => state.setCount);
 	const [score, setScore] = useState({ oScore: 0, xScore: 0 });
+
+	//zustan store board
 
 	//use slice and clone the Array to get befor and after each play
 	const handleClick = (idx) => {
 		const historyStep = history.slice(0, stepNum + 1);
 		const currentHistory = historyStep[stepNum];
 		const squares = [...currentHistory];
+
+		checkWiner();
 
 		//now we need to know if wine or its occupaid for no more click o to change the x for o.
 		if (winner || squares[idx]) return;
@@ -28,17 +34,22 @@ const Game = () => {
 		setHistory([...historyStep, squares]);
 		setStepNum([historyStep.length]);
 		setXIsNext(!xisNext);
+	};
 
+	//checkwinner to count +1
+	const checkWiner = () => {
 		let { oScore, xScore } = score;
-
-		if (winner === 'O') {
-			oScore++;
-			setScore({ ...score, oScore });
-		} else {
-			xScore++;
-			setScore({ ...score, xScore });
+		if (winner) {
+			if (winner === 'O') {
+				oScore += 1;
+				setScore({ oScore, xScore });
+				setCount({ oScore, xScore });
+			} else {
+				xScore += 1;
+				setScore({ oScore, xScore });
+				setCount({ oScore, xScore });
+			}
 		}
-		setCount({ oScore, xScore });
 	};
 
 	const jumpTo = (step) => {
@@ -46,10 +57,10 @@ const Game = () => {
 		setXIsNext(step % 2 === 0);
 	};
 
-	//this button help the player to move in each move played
+	//this button help the player to move in each move played, and remove all the itemns in the board but not the game
 	const moves = () =>
 		history.map((_step, move) => {
-			const playGame = move ? `Ir al movieminto #${move}` : `Re-iniciar juego`;
+			const playGame = move ? `Ir al movieminto #${move}` : `Jugar de nuevo `;
 			return (
 				<ol key={move}>
 					<button onClick={() => jumpTo(move)}>{playGame}</button>
@@ -57,13 +68,21 @@ const Game = () => {
 			);
 		});
 
-	console.log(winner);
-	console.log(count);
+	//reset game board and count
+	const resetBoard = () => {
+		let { oScore, xScore } = score;
+		oScore = null;
+		xScore = null;
+		setScore({ oScore, xScore });
+		setCount({ oScore, xScore });
+		setHistory([Array(9).fill(null)]);
+		setStepNum(0);
+	};
 
 	return (
 		<>
 			<h1>El Gato</h1>
-			<ScoreBoard scores={count} />
+			<ScoreBoard scores={count} onClick={resetBoard} />
 			<Board squares={history[stepNum]} onClick={handleClick} />
 			<div className="info-wrapper">
 				<div>
--- src/Components/ScoreComponent.js
+++ src/Components/ScoreComponent.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const ScoreBoard = ({ scores }) => {
+const ScoreBoard = ({ scores, onClick }) => {
 	const { xScore, oScore } = scores;
 	return (
 		<div className="winerTable">
 			<h2>Juegos Ganados</h2>
 			<h5>Jugador X: {xScore} </h5>
 			<h5>Jugador O: {oScore}</h5>
+			<button className="btn-retart" onClick={onClick}>
+				Reiniciar Jeugo
+			</button>
 		</div>
 	);
 };
